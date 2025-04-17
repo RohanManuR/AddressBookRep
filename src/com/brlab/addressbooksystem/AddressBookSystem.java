e0e6332d6621a34be6afcc17e943ba438251e532
rohan <rohanrmanu99@gmail.com>
2025-04-17 09:29:17 +0530

    Added AddressBookSystem project to main branch

---
+++ src/com/brlab/addressbooksystem/AddressBookSystem.java
@@ -0,0 +1,7 @@
+package com.brlab.addressbooksystem;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program in AddressBook Main class on Master Branch");
+    }
+}
